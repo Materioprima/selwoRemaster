e55e137ea08f6094b9f4e57bc9a656f5041e0670
Materio <mateoburbano03@gmail.com>
2018-02-03 04:28:57 +0100

    Segundo contenedor

--- hacks.js
+++ hacks.js
@@ -1,1 +1,22 @@
 AOS.init();
+
+var slider= ['img/s1.jpeg', 'img/s2.jpeg', 'img/s3.jpeg', 'img/s4.jpeg', 'img/s5.jpg'];
+var cont=0;
+
+$('.sliderder').click(function(){
+	cont++;
+	if(cont>4){
+		cont=0;
+	}
+	console.log(cont);
+	$('.fotoslider').attr('src',slider[cont]);
+});
+
+$('.sliderizq').click(function(){
+	cont--;
+	if(cont<0){
+		cont=4;
+	}
+	console.log(cont);
+	$('.fotoslider').attr('src',slider[cont]);
+});
